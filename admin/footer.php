dd6e8b262734edaf28e21e4c6319383e15ac01f6
pruthvi9053 <pruthvipatil9053@gmail.com>
2025-05-11 22:13:26 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,56 @@
+<div class="footer">
+  <div class="container">
+    <div class="col-md-3 ftr_navi ftr">
+      <h3>NAVIGATION</h3>
+      <ul>
+        <li>
+          <a href="../index.php">Home</a>
+        </li>
+        <li>
+          <a href="../student/login.php">Student Login</a>
+        </li>
+        <li>
+          <a href="../companies/login.php">Company Login</a>
+        </li>
+
+        <li>
+          <a href="login.php">Admin Login</a>
+        </li>
+      </ul>
+    </div>
+    <!-- <div class="col-md-3 ftr_navi ftr">
+      <h3>MEMBERS</h3>
+      <ul>
+        <li>
+          <a href="#">Customer Support</a>
+        </li>
+        <li>
+          <a href="#">Placement Support</a>
+        </li>
+        <li>
+          <a href="#">Faculty Support</a>
+        </li>
+        <li>
+          <a href="#">Registered Companies</a>
+        </li>
+        <li>
+          <a href="#">Training</a>
+        </li>
+      </ul>
+    </div> -->
+    <div class="col-md-3 get_in_touch ftr">
+    <h3>GET IN TOUCH</h3>
+      <p>ASHOKRAO MANE GROUP OF INSTITUTION</p>
+      <P> VATHAR,KOLHAPUR</P>
+      <a href="http://www.amgoi.org/">http://www.amgoi.org/</a>
+    </div>
+    <div class="col-md-3 get_in_touch ftr">
+
+    </div>
+    <div class="col-md-3 ftr-logo">
+      <p>Copyright &copy; AMG-2024
+
+    </div>
+    </div>
+
+  </div>
